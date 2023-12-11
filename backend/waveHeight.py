7b8190e76f731ce2ba9b2ecfb2270b962e2079ab
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2023-12-11 05:56:01 +0100

    fetch from backend

---
+++ backend/waveHeight.py
@@ -0,0 +1,11 @@
+import random
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def waveHeight():
+    return jsonify(height=random.randint(1, 10))
--- frontend/src/components/InteractiveMap.tsx
+++ frontend/src/components/InteractiveMap.tsx
@@ -1,7 +1,12 @@
-import { type Component, createSignal } from "solid-js";
+import { type Component, createSignal, createResource } from "solid-js";
 import MapGL, { Viewport, Control } from "solid-map-gl";
 import * as maplibre from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+
+type Wave = {
+	height: number;
+	unit: string;
+};
 
 const InteractiveMap: Component = () => {
 	const [viewport, setViewport] = createSignal({
@@ -9,9 +14,23 @@ const InteractiveMap: Component = () => {
 		zoom: 11,
 	} as Viewport);
 
-	const [waveHeight, setWaveHeight] = createSignal<string | number>(
+	const [location, setLocation] = createSignal<string | maplibre.LngLat>(
 		"Click on the location to show maximum wave height"
 	);
+
+	const fetchWaveHeight = async () => {
+		const response = await fetch(`http://127.0.0.1:5000`);
+		let height: number;
+		let unit: string;
+		if (response.ok) {
+			const data = (await response.json()) as Wave;
+			height = data.height;
+			unit = data.unit;
+		}
+		return height;
+	};
+	const [waveHeight] = createResource(location, fetchWaveHeight);
+
 	return (
 		<div class="w-full h-full">
 			<MapGL
@@ -22,14 +41,14 @@ const InteractiveMap: Component = () => {
 				onViewportChange={(evt: Viewport) => setViewport(evt)}
 				onClick={(evt: maplibre.MapMouseEvent) => {
 					console.log(evt.lngLat);
-					setWaveHeight(Math.random() * 10);
+					setLocation(evt.lngLat);
 				}}
 			>
 				<Control type="navigation" position="top-left" />
 				<Control type="fullscreen" position="top-right" />
 			</MapGL>
 			<div class="text-gray-50 text-center font-mono mt-5">Max wave height: </div>
-			<div class="text-gray-50 text-center font-mono mt-5 text-xl ">{waveHeight()} </div>
+			<div class="text-gray-50 text-center font-mono mt-5 text-xl ">{waveHeight()}</div>
 		</div>
 	);
 };
